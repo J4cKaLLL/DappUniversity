f390a1abae4938ab5073fba235e3d0af86eb7397
Suchito Makaroto <J4cKaL.r@gmail.com>
2020-01-08 15:54:15 -0500

    test

---
+++ test/MarketPlace.test.js
@@ -0,0 +1,22 @@
+const Marketplace = artifacts.require('./MarketPlace.sol')
+
+contract('Marketplace', (accounts) => {
+    let marketplace
+    before(async () => {
+        marketplace = await Marketplace.deployed()
+    })
+
+    describe('deployment', async () => {
+        it('deploys successfully', async () => {
+            const address = await marketplace.address
+            assert.notEqual(address, 0x0)
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+        })
+        it('has a name', async () =>{
+            const name = await marketplace.name()
+            assert.equal(name, 'Dapp University Marketplace')
+        })
+    })
+})

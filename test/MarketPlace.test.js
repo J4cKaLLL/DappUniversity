e6abf966820b0ee10327af5518581d98b22ba9c3
Suchito Makaroto <J4cKaL.r@gmail.com>
2020-01-10 16:50:17 -0500

    Update

--- test/MarketPlace.test.js
+++ test/MarketPlace.test.js
@@ -55,7 +55,40 @@ contract('Marketplace', ([deployer, seller, buyer]) => {
             assert.equal(product.purchased, false ,'purchased is correct') 
         })
         it('sells products', async () =>{
+            // Track the seller balance purchase
+            let oldSellerBalance
+            let deployerBalance
+            let sellerBalance
+            let buyerBalance
+            oldSellerBalance = await web3.eth.getBalance(seller)
+            console.log("oldSellerBalance = ",oldSellerBalance)
+            oldSellerBalance = new web3.utils.BN(oldSellerBalance)            
+            //SUCCESS: buyer makes purchase
             result = await marketplace.purchaseProduct(productCount, {from: buyer, value: web3.utils.toWei('1', 'Ether')})
+            deployerBalance = await web3.eth.getBalance(deployer)
+            sellerBalance = await web3.eth.getBalance(seller)
+            buyerBalance = await web3.eth.getBalance(buyer)
+            console.log(deployerBalance,sellerBalance,buyerBalance)
+            const event = result.logs[0].args    
+            assert.equal(event.id.toNumber(),productCount.toNumber(),'id is correct')
+            assert.equal(event.name, 'iPhone X','name is correct') 
+            assert.equal(event.price, '1000000000000000000','price is correct') 
+            assert.equal(event.owner, buyer ,'owner is correct') 
+            assert.equal(event.purchased, false ,'purchased is correct') 
+
+            //check that seller received funds
+            let newSellerBalance
+            newSellerBalance = await web3.eth.getBalance(seller)
+            console.log("newSellerBalance = ",newSellerBalance)
+            newSellerBalance = new web3.utils.BN(newSellerBalance)
+
+            let price
+            price = web3.utils.toWei('1', 'Ether')
+            console.log("price = ",price)
+            price = new web3.utils.BN(price)
+
+            const expectedBalance = oldSellerBalance.add(price)
+            assert.equal(newSellerBalance.toString(),expectedBalance.toString())
         })
     })
 })

74844a8262e9d2671d66f94f9a0b2bd5647906de
Suchito Makaroto <J4cKaL.r@gmail.com>
2020-01-10 14:33:35 -0500

    Parte 4

--- src/contracts/MarketPlace.sol
+++ src/contracts/MarketPlace.sol
@@ -10,20 +10,59 @@ contract MarketPlace {
         uint id;
         string name;
         uint price;
-        address owner;
+        address payable owner;
         bool purchased;
     }
+
+     event ProductCreated (
+        uint id,
+        string name,
+        uint price,
+        address payable owner,
+        bool purchased
+    );
+
+     event ProductPurchased (
+        uint id,
+        string name,
+        uint price,
+        address payable owner,
+        bool purchased
+    );
 
     constructor() public {
         name = "Dapp University Marketplace";
         eder = "Eder Rubiano";
     }
 
-    function createProduct() public {
-        // make sure parameters are correct
-        // Create the product
+    function createProduct(string memory _name, uint _price) public {
+        // Require a name
+        require(bytes(_name).length > 0, "Error1");
+        // Require a valid price
+        require(_price > 0,"Error2");
         // Increment product count
         productCount++;
+        // Create the product
+        products[productCount] = Product(productCount, _name, _price, msg.sender, false);
         // Trigger an event
+        emit ProductCreated(productCount, _name, _price, msg.sender, false);
+    }
+
+    function purchaseProduct(uint _id) public payable {
+        // Fetch the product
+        Product memory _product = products[_id];
+        // Fetch the owner
+        address payable _seller = _product.owner;
+        // Make sure product is valid        
+        // Transfer Ownership to the buyer
+        _product.owner = msg.sender;
+        // Mark as purchased
+        _product.purchased = true;
+        // Update the product
+        products[_id] = _product;
+        // Pay the seller by sending them ether
+        address(_seller).transfer(msg.value);
+        // Trigger an event
+        emit ProductPurchased(productCount, _product.name, _product.price, msg.sender, false);
     }
 }
--- test/MarketPlace.test.js
+++ test/MarketPlace.test.js
@@ -1,6 +1,10 @@
 const Marketplace = artifacts.require('./MarketPlace.sol')
 
-contract('Marketplace', (accounts) => {
+require('chai')
+.use(require('chai-as-promised'))
+.should()
+
+contract('Marketplace', ([deployer, seller, buyer]) => {
     let marketplace
     before(async () => {
         marketplace = await Marketplace.deployed()
@@ -23,12 +27,35 @@ contract('Marketplace', (accounts) => {
     describe('products', async () => {
         let result, productCount
         before(async () => {
-            result = await marketplace.createProduct()
+            result = await marketplace.createProduct('iPhone X', web3.utils.toWei('1','Ether'), { from: buyer })
             productCount = await marketplace.productCount()
         })
         it('creates products', async () =>{
             // SUCCESS
-            assert.equal(productCount,1)            
+            assert.equal(productCount,1)   
+            //console.log(result.logs)
+            //console.log("RESULTADOS")
+            const event = result.logs[0].args    
+            assert.equal(event.id.toNumber(),productCount.toNumber(),'id is correct')
+            assert.equal(event.name, 'iPhone X','name is correct') 
+            assert.equal(event.price, '1000000000000000000','price is correct') 
+            assert.equal(event.owner, buyer ,'owner is correct') 
+            assert.equal(event.purchased, false ,'purchased is correct') 
+        // FAILURE: Product must have a name
+        await await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), {from: seller}).should.be.rejected;
+        // FAILURE: Product must have a price
+        await await marketplace.createProduct('iPhone X', 0, {from: seller}).should.be.rejected;
+        })
+        it('lists products', async () =>{
+            const product = await marketplace.products(productCount)
+            assert.equal(product.id.toNumber(),productCount.toNumber(),'id is correct')
+            assert.equal(product.name, 'iPhone X','name is correct') 
+            assert.equal(product.price, '1000000000000000000','price is correct') 
+            assert.equal(product.owner, buyer ,'owner is correct') 
+            assert.equal(product.purchased, false ,'purchased is correct') 
+        })
+        it('sells products', async () =>{
+            result = await marketplace.purchaseProduct(productCount, {from: buyer, value: web3.utils.toWei('1', 'Ether')})
         })
     })
 })
